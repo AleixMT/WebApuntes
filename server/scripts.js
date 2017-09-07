386c5bcba1f8ead99693e0e44fc150957489fd56
Axlfc <axelfernandezcurros@gmail.com>
2017-09-08 01:36:23 +0200

    A ver qué dise jaja
    hola mi wapa

--- server/scripts.js
+++ server/scripts.js
@@ -1,3 +1,28 @@
+var price1 = 5;
+var price2 = 6;
+var total = price1 + price2;
+
 function Example(){
 	document.getElementById("demo").innerHTML = 5 + 6;
 };
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
